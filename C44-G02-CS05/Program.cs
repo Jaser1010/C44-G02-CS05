fbf88b6e556c8509433f136dea51b81bf3623958
Jaser1010 <j.osama2101@nu.edu.eg>
2025-07-22 21:01:44 +0300

    1. Write a program in C# Sharp to find the sum of all elements of the array.

--- C44-G02-CS05/Program.cs
+++ C44-G02-CS05/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace C44_G02_CS05
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace C44_G02_CS05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region 1. Write a program in C# Sharp to find the sum of all elements of the array.
+            /*
+            Console.Write("Enter the number of elements: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[n1];
+            int sum1 = 0;
+
+            Console.WriteLine("Enter the elements:");
+            for (int i = 0; i < n1; i++)
+            {
+                Console.Write($"Element {i}: ");
+                arr1[i] = int.Parse(Console.ReadLine());
+                sum1 += arr1[i];
+            }
+            Console.WriteLine($"The sum of all elements is: {sum1}");
+            */
+            #endregion
+
+
+
+
         }
     }
 }
